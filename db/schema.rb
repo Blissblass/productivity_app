596fc8b24894ff7b3259964c27257cee38962314
Lily <lolpo12345@outlook.com>
2021-11-24 17:23:02 +0300

    Create Devise User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_141318) do
+ActiveRecord::Schema.define(version: 2021_11_24_142009) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "description"
@@ -25,6 +25,19 @@ ActiveRecord::Schema.define(version: 2021_11_24_141318) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
