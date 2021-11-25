30eabe9a867ada262462b9c6e635b45b329f1c5c
Lily <lolpo12345@outlook.com>
2021-11-25 03:15:29 +0300

    Ensure server starts, create folders for Navbar

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home/Home.jsx';
 
 ReactDOM.render(
   <Router>
     <Routes>
-      <Route exact path="/" element={Home} />
+      <Route exact path="/" element={<Home />} />
     </Routes>
   </Router>,
   document.getElementById('root')
