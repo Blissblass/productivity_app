af4a383e5d52e166b6aad0a6be6b9ec04ab8a5b0
Lily <lolpo12345@outlook.com>
2021-12-05 20:55:42 +0300

    Add routes for Login and Signup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './stylesheets/custom.scss';
 import Home from './components/Home/Home.jsx';
 import NaviBar from './components/Navbar/Navibar.jsx';
+import SignUp from './components/SignUp/SignUp';
+import Login from './components/Login/Login';
 
 ReactDOM.render(
   <div>
@@ -10,6 +12,8 @@ ReactDOM.render(
       <NaviBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signUp" element={<SignUp />} />
       </Routes>
     </Router>
   </div>,
