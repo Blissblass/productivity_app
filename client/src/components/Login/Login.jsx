b29f0c8727e3b3a180c6eb4a08d90c73048d0fc0
Lily <lolpo12345@outlook.com>
2021-12-05 22:56:42 +0300

    Create log in page

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -2,7 +2,14 @@
 const Login = (props) => {
 
   return(
-    <h1>Login</h1>
+    <div className="card w-50 text-center mx-auto mt-5">
+    <h3 className="p-3">Log in to an existing account</h3>
+    <form className="p-3 pt-1">
+      <input type="text"  className="form-control form-control-lg mt-3" placeholder="E-Mail..." />
+      <input type="text"  className="form-control form-control-lg mt-3" placeholder="Password..." />
+      <button type="button" className="btn btn-primary btn-lg text-white mt-3 w-50">Log In</button>
+    </form>
+  </div>
   )
 };
 
