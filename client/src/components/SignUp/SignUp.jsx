6bbaca00def8d2f4eb7a178334db8983c67f76cf
Lily <lolpo12345@outlook.com>
2021-12-05 23:51:42 +0300

    Add handleSignUp function

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,15 +1,34 @@
 
 const SignUp = (props) => {
 
+  const handleSignUp = (e) => {
+    e.preventDefault();
+
+    const targets = [...e.currentTarget.children].map(child => child.value);
+    const data = {
+      username: targets[0],
+      email: targets[1],
+      password: targets[2],
+      password_confirmation: targets[3]
+    }
+    console.log(data);
+
+    // fetch('127.0.0.1:3001/users', {
+    //   method: 'POST',
+    //   headers: new Headers({'Content-Type': 'application/json'}),
+    //   body: data
+    // });
+  };
+
   return(
     <div className="card w-50 text-center mx-auto mt-5">
       <h3 className="p-3">Join Nook.io today!</h3>
-      <form className="p-3 pt-1">
+      <form className="p-3 pt-1" onSubmit={handleSignUp}>
         <input type="text"  className="form-control form-control-lg" placeholder="Username..." />
         <input type="text"  className="form-control form-control-lg mt-3" placeholder="E-Mail..." />
-        <input type="text"  className="form-control form-control-lg mt-3" placeholder="Password..." />
-        <input type="text"  className="form-control form-control-lg mt-3" placeholder="Password Confirmation..." />
-        <button type="button" className="btn btn-primary btn-lg text-white mt-3 w-50">Sign Up</button>
+        <input type="password"  className="form-control form-control-lg mt-3" placeholder="Password..." />
+        <input type="password"  className="form-control form-control-lg mt-3" placeholder="Password Confirmation..." />
+        <button type="submit" className="btn btn-primary btn-lg text-white mt-3 w-50">Sign Up</button>
       </form>
     </div>
   )
