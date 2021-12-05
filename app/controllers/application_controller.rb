dee89a22ed7219ac1efb5303ceb7d49f68913c92
Lily <lolpo12345@outlook.com>
2021-12-06 00:01:00 +0300

    Configure devise to allow the :username parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_paramater_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+    devise_paramater_sanitizer.permit(:sign_in, keys: [:username, :email, :password])
+  end
 end
