296f6e95f77eb0f89ad6ba9c7738c7ffb7a0f6a2
Lily <lolpo12345@outlook.com>
2021-12-06 00:45:00 +0300

    Complete login and sign up functionality

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -11,7 +11,16 @@ const Login = (props) => {
       }
     }
 
-    console.log(data);
+    fetch("users/sign_in", {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }),
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(res => console.log(res))
   };
 
   return(
@@ -19,7 +28,7 @@ const Login = (props) => {
     <h3 className="p-3">Log in to an existing account</h3>
     <form className="p-3 pt-1" onSubmit={handleLogin}>
       <input type="text"  className="form-control form-control-lg mt-3" placeholder="E-Mail..." />
-      <input type="text"  className="form-control form-control-lg mt-3" placeholder="Password..." />
+      <input type="password"  className="form-control form-control-lg mt-3" placeholder="Password..." />
       <button type="submit" className="btn btn-primary btn-lg text-white mt-3 w-50">Log In</button>
     </form>
   </div>
