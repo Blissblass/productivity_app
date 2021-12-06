f3c4a3b671577bfe784386b3d17bc09e6e8c3765
Lily <lolpo12345@outlook.com>
2021-12-06 18:51:13 +0300

    Add UserContext to app

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,18 +5,24 @@ import Home from './components/Home/Home.jsx';
 import NaviBar from './components/Navbar/Navibar.jsx';
 import SignUp from './components/SignUp/SignUp';
 import Login from './components/Login/Login';
+import UserContext from './components/Contexts/UserContext';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')))
 
   return(
   <div>
     <Router>
-      <NaviBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signUp" element={<SignUp />} />
-      </Routes>
+      <UserContext.Provider value={{user, setUser}}>
+        <NaviBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signUp" element={<SignUp />} />
+        </Routes>
+      </UserContext.Provider>
     </Router>
   </div>
   )
