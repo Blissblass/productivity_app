3f2e8e37de2ca0bdf6b791771209b102635afac3
Lily <lolpo12345@outlook.com>
2021-12-07 20:33:20 +0300

    Add missing to_do fetching to api:user#fetch_user

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -4,6 +4,6 @@ class Api::UserController < ApplicationController
     return { msg: "Please pass in a user id!" } unless params[:id]
 
     @user = User.includes(:to_dos).find(params[:id])
-    render json: @user
+    render json: { user: @user, lists: @user.to_dos }
   end
 end
