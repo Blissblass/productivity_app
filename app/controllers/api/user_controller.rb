f50430765210342339b373b3f24cc6fc73f35201
Lily <lolpo12345@outlook.com>
2021-12-07 21:19:21 +0300

    Add loading to user profile fetching

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,9 +1,9 @@
 class Api::UserController < ApplicationController
 
-  def fetch_user
+  def fetch_user_lists
     return { msg: "Please pass in a user id!" } unless params[:id]
 
     @user = User.includes(:to_dos).find(params[:id])
-    render json: { user: @user, lists: @user.to_dos }
+    render json: @user.to_dos
   end
 end
--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,19 +1,30 @@
-const { useEffect } = require("react");
+import { Spinner } from "react-bootstrap";
+
+const { useEffect, useState } = require("react");
 const { useParams } = require("react-router");
 
 const Profile = () => {
   const params = useParams();
+  const [isLoading, setLoading] = useState(true);
+  const [userLists, setUserLists] = useState([]);
 
   useEffect(() => {
-    fetch(`/api/user_profile/${params.id}`)
+    fetch(`/api/user_lists/${params.id}`)
       .then(res => res.json())
-      .then(userData => console.log(userData))
+      .then(userData => {
+        console.log(userData)
+        setUserLists(userData);
+        setLoading(false);
+      })
   }, [params]);
 
   return(
-    <div>
-      <h1>{}</h1>
-    </div>
+    isLoading ? 
+      <div className="spinner-border text-primary"></div>
+    :
+      <div>
+        <h1>{}</h1>
+      </div>
   )
 };
 
