4aca8b10d3e21b1a2b5e6c60db5766ea37e75d7a
Lily <lolpo12345@outlook.com>
2021-12-09 22:02:50 +0300

    Create code for ToDoController

--- app/controllers/toDo_controller.rb
+++ app/controllers/toDo_controller.rb
@@ -1,3 +1,40 @@
 class ToDoController < ApplicationController 
-  
+
+  def new
+    @to_do = ToDo.new
+  end
+
+  def create
+    @to_do = ToDo.new(to_do_params)
+    if @to_do.save 
+      render json: @to_do 
+    else
+      render json: @to_do.errors.full_messages
+    end
+  end
+
+  def edit 
+    @to_do = ToDo.find(params[:id])
+  end
+
+  def update 
+    @to_do = ToDo.find(params[:id])
+    if @to_do.update(to_do_params)
+      render json: @to_do 
+    else
+      render json: @to_do.errors.full_messages 
+    end
+  end
+
+  def destroy 
+    @to_do = ToDo.find(params[:id])
+    @to_do.destroy unless @to_do.nil?
+  end
+
+  private
+
+  def to_do_params 
+    params.require(:to_do).permit(:name, :user_id)
+  end
+
 end
