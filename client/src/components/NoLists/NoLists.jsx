234b1a3e8e7aaadb05b9df8977e5ca77b9a9a613
Lily <lolpo12345@outlook.com>
2021-12-09 21:35:26 +0300

    Create ListModal

--- client/src/components/NoLists/NoLists.jsx
+++ client/src/components/NoLists/NoLists.jsx
@@ -5,7 +5,7 @@ const NoLists = () => {
     <div className="mt-3">
       <div>
         <h3>No To-Do's found!</h3>
-        <input type="button" className="btn btn-primary text-white" value="Create a list now" />
+        <input type="button" className="btn btn-primary w-25 p-2 text-white" value="Create a list now" />
       </div>
     </div>
   );
