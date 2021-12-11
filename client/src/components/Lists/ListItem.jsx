2489f12f10ecc1fa609a04f6b5c918162d3d0446
Lily <lolpo12345@outlook.com>
2021-12-11 22:59:42 +0300

    Add link to list item

--- client/src/components/Lists/ListItem.jsx
+++ client/src/components/Lists/ListItem.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsPencilSquare } from 'react-icons/bs';
 import { CgTrashEmpty } from 'react-icons/cg';
 import { AiOutlineCheck } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router';
 
 const ListItem = (props) => {
   const { item, setUserLists } = props;
   const [editing, setEditing] = useState(false);
   const [editData, setEditData] = useState(item.name);
+  const { id } = useParams();
 
   const handleDelete = () => {
     fetch(`/to_do/${item.id}`, {
@@ -49,23 +52,25 @@ const ListItem = (props) => {
   const handleToggle = () => setEditing(old => !old);
 
   return(
-    <div className="mt-3">
-      <div className="card w-50 mx-auto">
-        <div className="d-flex justify-content-between align-items-center">
-          <div className="ms-3 mt-2 d-flex align-items-center">
-            <h3 style={{display: editing ? "none" : "block"}}>{item.name}</h3>
-            <input type="text" className="form-control form-control-lg mb-2" value={editData} placeholder="Name..." 
-              style={{display: editing ? "block" : "none"}} onChange={(e) => setEditData(e.target.value)} />
-            <AiOutlineCheck className="text-primary ms-1 mb-2" 
-              style={{fontSize: 40, cursor:"pointer", display: editing ? "block" : "none"}} onClick={handleEdit} />  
-          </div>
-          <div>
-            <BsPencilSquare className="text-primary me-3" style={{fontSize: 30, cursor:"pointer"}} onClick={handleToggle} />
-            <CgTrashEmpty className="text-primary me-2" style={{fontSize: 35, cursor:"pointer"}} onClick={handleDelete} />
+    <Link to={`list/${item.id}`}>
+      <div className="mt-3">
+        <div className="card w-50 mx-auto">
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="ms-3 mt-2 d-flex align-items-center">
+              <h3 style={{display: editing ? "none" : "block"}}>{item.name}</h3>
+              <input type="text" className="form-control form-control-lg mb-2" value={editData} placeholder="Name..." 
+                style={{display: editing ? "block" : "none"}} onChange={(e) => setEditData(e.target.value)} />
+              <AiOutlineCheck className="text-primary ms-1 mb-2" 
+                style={{fontSize: 40, cursor:"pointer", display: editing ? "block" : "none"}} onClick={handleEdit} />  
+            </div>
+            <div>
+              <BsPencilSquare className="text-primary me-3" style={{fontSize: 30, cursor:"pointer"}} onClick={handleToggle} />
+              <CgTrashEmpty className="text-primary me-2" style={{fontSize: 35, cursor:"pointer"}} onClick={handleDelete} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 };
 
