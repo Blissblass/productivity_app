bbc9f306dd84b9557c993c1e4855e196198cc12f
Lily <lolpo12345@outlook.com>
2021-12-13 00:37:42 +0300

    Change icon to button

--- client/src/components/CreateTask/CreateTask.jsx
+++ client/src/components/CreateTask/CreateTask.jsx
@@ -3,16 +3,13 @@ import { BsJournalPlus } from 'react-icons/bs';
 const CreateTask = () => {
 
   return(
-    <div className="card mx-auto w-75 p-3">
-      <div className=" d-flex  align-items-center justify-content-between">
-        <h5 className="m-0">Add new task</h5>
-        <div className="w-75">
-          <form className="d-flex align-items-center justify-content-around">
-            <input type="text" className="form-control form-control-lg w-75 ms-5" placeholder="Task..." required />
-            <BsJournalPlus style={{fontSize: 40}} className="me-3 text-primary" />
+    <div className="mx-auto w-75 p-2">
+        <div >
+          <form className="d-flex align-items-center justify-content-center">
+            <input type="text" className="form-control form-control-lg w-75 me-3" placeholder="Add a new task..." required />
+            <button style={{backgroundColor: "transparent", border: "none"}}><BsJournalPlus style={{fontSize: 37, cursor: "pointer"}} className="text-primary" /></button>
           </form>
         </div>
-      </div>
     </div>
   )
 };
