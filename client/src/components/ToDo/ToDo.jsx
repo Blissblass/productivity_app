ab48056c7eb854e7d615ed202e767860f149220c
Lily <lolpo12345@outlook.com>
2021-12-12 18:46:15 +0300

    Add warning for no tasks

--- client/src/components/ToDo/ToDo.jsx
+++ client/src/components/ToDo/ToDo.jsx
@@ -5,19 +5,38 @@ import { useParams } from 'react-router';
 
 const ToDo = () => {
   const params = useParams();
-  const [listData, setListData] = useState({});
+  const [listData, setListData] = useState({list: null, tasks: null});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     console.log(params);
     fetch(`/api/todo/${params.list_id}`)
       .then(res => res.json())
-      .then(data => console.log(data));
-  }, []);
+      .then(data => {
+        console.log(data);
+        setListData(data);
+        setIsLoading(false);
+      });
+  }, [params]);
 
   return(
-    <div className="container">
-      <h1><ReactMarkdown>*Test*</ReactMarkdown></h1>
-      <ReactMarkdown>---</ReactMarkdown>
+    <div>
+      { isLoading ? 
+        <div className="spinner-border text-primary d-block mx-auto mt-4" style={{height: 150, width: 150}}></div>
+      :
+        <div>
+          <div className="container text-center mt-3">
+            <h1><ReactMarkdown children={`*${listData.list.name}*`}></ReactMarkdown></h1>
+            <ReactMarkdown className="text-primary">---</ReactMarkdown>
+          </div>
+          { 
+          listData.length ?
+            null 
+          :
+            <h1>No tasks found!</h1>  
+          }
+        </div>
+      }
     </div>
   )
 };
