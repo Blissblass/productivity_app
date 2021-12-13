ce9b59b795c6bd88269f2a25f0e9aa520b963e15
Lily <lolpo12345@outlook.com>
2021-12-14 00:44:01 +0300

    Remove debugging code

--- client/src/components/Task/Task.jsx
+++ client/src/components/Task/Task.jsx
@@ -5,7 +5,7 @@ const Task = (props) => {
 
   return(
     <li className="list-group-item list-group-item-primary m-1">
-      <h4 className="m-1">{task.description}{task.id}</h4>
+      <h4 className="m-1">{task.description}</h4>
     </li>
   )
 };
