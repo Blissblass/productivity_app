e8a7b623d9a1d2f6d93295d72b38156ab5a550c5
Lily <lolpo12345@outlook.com>
2021-12-15 17:17:20 +0300

    Fix missing required on email field

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, length: { minimum: 5, maximum: 15 }, uniqueness: true
+  validates :email, presence: true
   has_many :to_dos
 end
