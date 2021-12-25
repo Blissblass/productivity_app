2d69c4f2edd8700c6491550679131edc4b6655e2
Lily <lolpo12345@outlook.com>
2021-12-25 19:28:22 +0300

    Test workaround for heroku failing to update records

--- app/controllers/api/toDo_controller.rb
+++ app/controllers/api/toDo_controller.rb
@@ -12,5 +12,8 @@ class Api::ToDoController < ApplicationController
   def reorder_list
     order_data = params[:to_do][:orderData]
     Task.update(order_data.keys, order_data.values)
+    order_data.keys.each do |task_idx|
+      Task.update(task_idx, order_data[task_idx])
+    end
   end
 end
