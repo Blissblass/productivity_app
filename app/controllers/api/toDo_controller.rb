cc2ddbf00b3be50872b8457baaafe4f9e61f8928
Lily <lolpo12345@outlook.com>
2021-12-25 19:32:58 +0300

    Test fix for wrong ordering of tasks

--- app/controllers/api/toDo_controller.rb
+++ app/controllers/api/toDo_controller.rb
@@ -3,7 +3,7 @@ class Api::ToDoController < ApplicationController
   def fetch_list 
     @to_do = ToDo.includes(:tasks).find(params[:id])
     if @to_do 
-      render json: { list: @to_do, tasks: @to_do.tasks.order(index: :desc) } 
+      render json: { list: @to_do, tasks: @to_do.tasks.order(:index) } 
     else
       render json: @to_do.errors.full_messages 
     end
